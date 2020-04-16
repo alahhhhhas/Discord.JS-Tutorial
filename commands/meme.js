504842f628a3d37485fcdad2174b8fdcd98fa677
alahhhhhas <61631355+alahhhhhas@users.noreply.github.com>
2020-04-16 14:42:42 +0200

    Update meme.js

--- commands/meme.js
+++ commands/meme.js
@@ -5,24 +5,18 @@ const superagent = require("superagent")
 
 
 module.exports.run = async (bot, message, args) => {
-    let msg = await message.channel.send("Generating...")
+    run: async (client, message, args) => {
+        const subReddits = ["dankmeme", "meme", "me_irl"];
+        const random = subReddits[Math.floor(Math.random() * subReddits.length)];
+        const img = await randomPuppy(random);
+        const embed = new RichEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`From /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
 
-    let {body} = await superagent
-    .get(`https://api-to.get-a.life/meme`)
-    //console.log(body.file)
-    if(!{body}) return message.channel.send("I broke! Try again.")
-
-        let mEmbed = new Discord.RichEmbed()
-        .setColor(colours.cyan)
-        .setAuthor(`TestBot MEMES!`, message.guild.iconURL)
-        .setImage(body.url)
-        .setTimestamp()
-        .setFooter(`TEST BOT`, bot.user.displayAvatarURL)
-
-        message.channel.send({embed: mEmbed})
-
-        msg.delete();
-}
+        message.channel.send(embed);
+};
 
 
 module.exports.config = {
